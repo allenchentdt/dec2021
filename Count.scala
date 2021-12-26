4b477ab0480a062f79d441a93ba7882325b1e4b7
allenchentdt <95040833+allenchentdt@users.noreply.github.com>
2021-12-26 12:05:34 +0700

    Update Count.scala

--- Count.scala
+++ Count.scala
@@ -7,20 +7,12 @@
     def main(args: Array[String]) {
       var output = new ListBuffer[Int]()
       val input = Source.fromFile("input.txt").getLines.toArray
-      /*Lấy ra số testcase*/
       val T = input(0).toInt
       for (t <- 1 to T) {
-        /*Lấy ra từng mảng của mỗi testcase*/
-        val A = input(t * 2).split("\\D+").filter(_.nonEmpty).toList
-        /*Nhóm các giá trị giống nhau lại và đếm số lượng trùng lặp*/
-        val hm = A.groupBy(a => a).map(v => (v._1, v._2.length - 1))
-        /*Tính tổng số lượng trùng lặp cần phải xử lý*/
-        var count = hm.values.sum
-        output += count
+        output += input(t * 2).split("\\D+").filter(_.nonEmpty).toList.groupBy(a => a).map(v => (v._1, v._2.length -1)).values.sum
       }
       val writer = new PrintWriter(new File("output.txt"))
       writer.write(output.mkString("\n"))
       writer.close()
-      Source.fromFile("output.txt").foreach { x => print(x) }
     }
   }
